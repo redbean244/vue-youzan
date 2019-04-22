b0cbce7a6303fdb88a254e13de8ff202ac1dfb71
redbean244 <798388771@qq.com>
2019-04-22 20:59:00 +0800

    添加Infinite scroll 效果

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,20 +5,43 @@ import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
 
-
+import { InfiniteScroll } from 'mint-ui'
+Vue.use(InfiniteScroll);
 
 let app = new Vue({
     el: "#app",
     data:{
-        lists: null
+        lists: null,
+        pageNum: 1,
+        pageSize: 6,
+        loading: false,
+        allLoaded: false
+
     },
     created() {
-        axios.get(url.hotLists,{
-            pageNum: 1,
-            pageSize: 6
-        }).then(res =>{
-            this.lists = res.data.lists;
-            console.log(res);
-        })
+        this.getLists()
+    },
+    methods:{
+        getLists(){ 
+            if (this.allLoaded) return;
+            this.loading = true;
+            axios.get(url.hotLists,{
+                pageNum: this.pageNum,
+                pageSize: 6
+            }).then(res =>{
+                let curLists = res.data.lists;
+                //判断所有数据是否加载完毕
+                if(curLists.length<this.pageSize){
+                    this.allLoaded = true;
+                }
+                if(this.lists){
+                    this.lists = this.lists.concat(curLists)
+                }else{
+                    this.lists = curLists
+                }
+                this.loading = false;
+                this.pageNum++;
+            })
+        }
     }
 })
