02937a01737b6d2b22701b4db4caa72a4ee6d2ca
redbean244 <798388771@qq.com>
2019-05-07 14:27:15 +0800

    购物车选择实现

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,3 +1,38 @@
 import "./cart_base.css"
 import "./cart_trade.css"
 import "./cart.css"
+
+import Vue from 'vue'
+import axios from 'axios'
+import url from 'js/api.js'
+
+
+import Foot from 'components/Foot.vue'
+import Swipe from 'components/Swipe.vue'
+
+let app = new Vue({
+    el: ".container",
+    data:{
+        lists:null
+    },
+    created(){
+        this.getList();
+    },
+    methods:{
+        getList(){
+            axios.get(url.cartLists).then(res=>{
+                let lists=res.data.cartList;
+                lists.forEach(shop=>{
+                    shop.checked = true;
+                    shop.goodsList.forEach(good=>{
+                        good.checked = true;
+                    })
+                })
+                this.lists = lists;
+            })
+        },
+        selectGood(good){
+            good.checked = !good.checked;
+        }
+    }
+})
