686099b85a4fb67d4465e626a0af3168c721e5a2
redbean244 <798388771@qq.com>
2019-05-18 02:08:34 +0800

    路由配置二

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -3,11 +3,26 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 import Member from './components/member.vue'
-
+import Address from './components/address.vue'
+import All from './components/all.vue'
+import Form from './components/from.vue'
 
 let routes =[{
     path:'/',
     component: Member
+},{
+    path: '/address',
+    component: Address,
+    children:[{
+        path: '',
+        redirect:'All'
+    },{
+        path: 'all',
+        component: All
+    },{
+        path: 'form',
+        component: Form
+    }]
 }]
 
 let router = new Router({
