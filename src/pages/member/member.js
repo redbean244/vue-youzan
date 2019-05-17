1334f6fb90a3ad9571294bb55f1bd011df5b2eb6
redbean244 <798388771@qq.com>
2019-05-17 22:27:08 +0800

    路由配置一

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,2 +1,20 @@
-import "./member_base.css"
-import "./member.css"
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+
+import Member from './components/member.vue'
+
+
+let routes =[{
+    path:'/',
+    component: Member
+}]
+
+let router = new Router({
+    routes
+})
+
+new Vue({
+    el:"#app",
+    router
+})
